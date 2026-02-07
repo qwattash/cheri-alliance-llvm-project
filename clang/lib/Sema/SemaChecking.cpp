25bf65eedde8888f896e8b071cfd1554a07b37cc
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-02-07 14:07:28 +0000

    [Clang] Re-enable cheri_tag_clear builtin in Clang.

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -291,7 +291,6 @@ static bool SemaVerifyBakewellBuiltins(Sema &S, unsigned BuiltinID, CallExpr *Th
   case Builtin::BI__builtin_cheri_cap_type_copy:
   case Builtin::BI__builtin_cheri_unseal:
   case Builtin::BI__builtin_cheri_conditional_seal:
-  case Builtin::BI__builtin_cheri_tag_clear:
   case Builtin::BI__builtin_cheri_seal:
   case Builtin::BI__builtin_cheri_cap_load_tags:
     S.Diag(TheCall->getBeginLoc(), diag::err_bakewell_unsupported_builtin)
